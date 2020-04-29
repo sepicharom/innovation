d1d92442535b23e8cf60cba949bacfdcf5681845
Samantha Salley <samanthasalley@gmail.com>
2020-04-28 21:26:36 -0700

    (import) cards data

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,22 +1,14 @@
-import React, {  } from 'react';
-import './Game.css';
+import React, { useState } from 'react';
 
-const roomNames = [
-  'a',
-  'b',
-  'c',
-];
-const getRandomInteger = (min, max) => {
-  const randomInt = Math.floor(Math.random() * (max - min + 1) + min);
-  return randomInt;
-};
-const ROOM_NAME = roomNames[getRandomInteger(0, roomNames.length - 1)];
+import cards from '../../utils/cards/index';
+
+import './Game.css';
 
 
 const Game = () => {
   return (
     <div>
-      <h1>Room Name: {ROOM_NAME}</h1>
+
     </div>
   );
 }
