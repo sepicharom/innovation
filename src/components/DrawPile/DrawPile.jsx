f28675918c1d66e927504cdf75bda7e573ea27d7
Samantha Salley <samanthasalley@gmail.com>
2020-05-19 21:37:41 -0700

    (add)  ✅  proptypes; (add)  ✨  disabled prop; (update)  🎨  div to Button

--- src/components/DrawPile/DrawPile.jsx
+++ src/components/DrawPile/DrawPile.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import Button from '../../libs/ui/Button/Button';
 
 const DrawPile = ({
   age,
   draw,
+  disabled,
   numCardsInPile,
 }) => {
   if (!numCardsInPile || numCardsInPile < 1) {
     return (<div>No more {age} age.</div>);
   }
   return (
-    <div onClick={() => draw(age)}>
+    <Button theme="transparent" disabled={disabled} onClick={() => draw(age)}>
       <h3>{age}</h3>
-    </div>
+    </Button>
   );
 }
+
+DrawPile.propTypes = {
+  age: PropTypes.number.isRequired,
+  draw: PropTypes.func.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  numCardsInPile: PropTypes.number.isRequired,
+};
 
 export default DrawPile;
