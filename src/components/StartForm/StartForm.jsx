5f668737af404429a4a44a72886d9b4d7744e214
Samantha Salley <samanthasalley@gmail.com>
2020-06-01 00:54:31 -0700

    (refactor)  ♻️  to use new RFF component templates (styling)  🎨  add styled components UI

--- src/components/StartForm/StartForm.jsx
+++ src/components/StartForm/StartForm.jsx
@@ -4,32 +4,70 @@ import { Form, Field } from 'react-final-form';
 import { ARRAY_ERROR } from 'final-form';
 import arrayMutators from 'final-form-arrays';
 import { FieldArray } from 'react-final-form-arrays';
+import { minChar, required } from '../../libs/forms/utils/validation';
+
+import RenderText from '../../libs/forms/fields/RenderText/RenderText';
+
+import styled from 'styled-components/macro';
+
+const StyledPlayerInput = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 2rem 0;
+`;
+
+const StyledRemove = styled.div`
+  font-size: 1.2rem;
+  margin-left: 8px;
+`;
+
+const StyledError = styled.div`
+  color: red;
+  font-weight: var(--font-weight-bold);
+  font-size: var(--font-size-sm);
+`;
+
+const BASE_PLAYER = { username: '' };
+
+const createNewPlayer = () => Object.assign({}, BASE_PLAYER);
+
+const min2Char = minChar(2);
+
+const INITIAL_PLAYERS = [createNewPlayer(), createNewPlayer()];
+
+const validateForm = (values) => {
+  const errors = {};
+  if (!values.players || values.players.length < 2) {
+    errors.players = [];
+    errors.players[ARRAY_ERROR] = 'Must have at least 2 players.';
+  } else if (values.players && values.players.length > 4) {
+    errors.players = [];
+    errors.players[ARRAY_ERROR] = 'Cannot have more than 4 players.';
+  }
+  return errors;
+};
+
+const validateUsername = (value) => {
+  const err = required(value) || min2Char(value) || undefined;
+  return err;
+};
 
 const StartForm = ({ onSubmit }) => {
   return (
     <Form
+      initialValues={{ players: [...INITIAL_PLAYERS] }}
       onSubmit={onSubmit}
-      validate={values => {
-        const errors = {}
-        if (!values.players || values.players.length < 2) {
-          errors.players = [];
-          errors.players[ARRAY_ERROR] = 'Must have at least 2 players.';
-        }
-        else if (values.players.length > 4) {
-          errors.players = [];
-          errors.players[ARRAY_ERROR] = 'Cannot have more than 4 players.';
-        }
-        return errors;
-      }}
+      validate={validateForm}
       mutators={{
         // enable FieldArray components
-        ...arrayMutators
+        ...arrayMutators,
       }}
       render={({
         handleSubmit,
         values,
         form: {
-          mutators: { push, pop }
+          mutators: { push, pop },
         }, // injected from final-form-arrays above
         form,
         pristine,
@@ -43,7 +81,7 @@ const StartForm = ({ onSubmit }) => {
                 type="button"
                 onClick={() => {
                   if (!values.players || values.players.length < 4) {
-                    push('players', undefined);
+                    push('players', createNewPlayer());
                   }
                 }}
               >
@@ -51,43 +89,44 @@ const StartForm = ({ onSubmit }) => {
               </button>
             </div>
             <FieldArray name="players">
-              {({ fields, meta: { error } }) =>
+              {({ fields, meta: { error } }) => (
                 <div>
                   {fields.map((name, index) => (
-                    <div key={name}>
-                      <label>Player {index + 1}</label>
+                    <StyledPlayerInput key={name}>
                       <Field
                         name={`${name}.username`}
-                        component="input"
-                        placeholder="username"
+                        label={`Player ${index + 1}`}
+                        component={RenderText}
+                        validate={validateUsername}
                       />
-                      <span
-                        onClick={() => fields.remove(index)}
-                        style={{ cursor: 'pointer' }}
-                        aria-label="Close"
-                        role="img"
-                      >
-                        ❌
-                    </span>
-                    </div>
+                      {index > 1 && (
+                        <StyledRemove
+                          onClick={() => fields.remove(index)}
+                          style={{ cursor: 'pointer' }}
+                        >
+                          <span aria-label="Remove Player" role="img">
+                            ❌
+                          </span>
+                        </StyledRemove>
+                      )}
+                    </StyledPlayerInput>
                   ))}
-                  {error && !pristine && <div><span>{error}</span></div>}
+                  {typeof error === 'string' && !pristine && (
+                    <StyledError>{error}</StyledError>
+                  )}
                 </div>
-              }
+              )}
             </FieldArray>
 
-            <button
-              type="submit"
-              disabled={submitting || pristine}
-            >
+            <button type="submit" disabled={submitting || pristine}>
               Let's Play!
             </button>
           </form>
-        )
+        );
       }}
     />
-  )
-}
+  );
+};
 
 StartForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
