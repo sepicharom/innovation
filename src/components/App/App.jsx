977a8c7e562de2dec84dd687a55822f569a39cc2
Samantha Salley <samanthasalley@gmail.com>
2020-06-08 17:46:01 -0700

    (install)  ✨  & configure react-router-dom for client-side routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,40 +1,27 @@
+/**
+ * @module  App
+ * @author  samanthasalley
+ * @description Top level component.
+ *              Gets hooked into our html in index.js.
+ *              Control point for top level routing.
+ */
+
 import React from 'react';
-import { connect } from 'react-redux';
-import * as playerActions from '../../actions/playerActions';
+import { Switch, Route } from 'react-router-dom';
 
 import Game from '../Game/Game';
-import StartForm from '../StartForm/StartForm';
+import Start from '../Start/Start';
 import PageWrapper from '../../libs/ui/PageWrapper/PageWrapper';
 
-const STARTING_PLAYER = Object.freeze({
-  username: null,
-  age: 1,
-});
-
-const mapStateToProps = (store) => ({
-  usernames: store.players.usernames,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setPlayers: (players) => dispatch(playerActions.setPlayers(players)),
-});
-
-const App = ({ usernames, setPlayers }) => {
-  const handleStartFormSubmit = (values) => {
-    const playersObj = values.players.reduce((obj, username) => {
-      const newPlayer = Object.assign({}, STARTING_PLAYER, { username });
-      obj[username] = newPlayer;
-      return obj;
-    }, {});
-    setPlayers(playersObj);
-  };
-
+const App = () => {
   return (
     <PageWrapper>
-      {!usernames.length && <StartForm onSubmit={handleStartFormSubmit} />}
-      {usernames.length > 0 && <Game />}
+      <Switch>
+        <Route exact path="/" component={Start} />
+        <Route exact path="/room/:gameId" component={Game} />
+      </Switch>
     </PageWrapper>
   );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store';
 import './libs/ui/styles/reset.css';
 import './libs/ui/styles/variables.css';
@@ -12,7 +13,9 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
