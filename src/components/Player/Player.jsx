906d44186a31f5e954b454e2ebba50f84f13301d
Samantha Salley <samanthasalley@gmail.com>
2020-06-23 11:12:58 -0700

    (update) Player to render new Board component

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Hand from '../Hand/Hand';
+import Board from '../Board/Board';
 
 const Player = ({ name }) => {
   return (
     <div>
       <h2>{name}</h2>
       <Hand player={name} />
+      <Board player={name} />
     </div>
   );
 };
